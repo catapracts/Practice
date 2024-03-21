bac81b375c2f5b409c2a650fe17a2f2c08f5db0a
aa <a@a>
2024-03-21 15:48:16 +0900

    Python - Class & Object Update

--- Python/Class_Object.py
+++ Python/Class_Object.py
@@ -89,7 +89,6 @@ import math
 
 
 class Animal:
-
     def __init__(self, age, height, color, xpos, ypos):
         self.age = age
         self.height = height
@@ -97,19 +96,14 @@ class Animal:
         self.xposition = xpos
         self.yposition = ypos
         self.velocity = 0
-
     def sound(self):
         pass
 
-
 class Horse(Animal):
-
     def __init__(self, age, height, color, xpos, ypos):
         Animal.__init__(self, age, height, color, xpos, ypos)
-
     def sound(self):
         print('Neigh')
-
     def run(self, xdistance, ydistance, time):
         self.xposition += xdistance
         self.yposition += ydistance
@@ -118,13 +112,10 @@ class Horse(Animal):
 
 
 class Dog(Animal):
-
     def __init__(self, age, height, color, xpos, ypos):
         Animal.__init__(self, age, height, color, xpos, ypos)
-
     def sound(self):
         print('Bow-Wow')
-
 
 if __name__ == '__main__':
     danbi = Horse(5, 160, 'brown', 0, 0)
